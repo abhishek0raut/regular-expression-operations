7a44f0587c7a1ad5ca3944179aea4356252eca3a
Abhishek Dinkar Raut <9390583+abhishek0raut@users.noreply.github.com>
2017-11-16 23:36:34 -0500

    Update regularExpressionOperations.py

--- regularExpressionOperations.py
+++ regularExpressionOperations.py
@@ -9,3 +9,23 @@ def example_1():
     hello_regex = re.compile(r,'Hello world')
     result = hello_regex.match('Hello world !')
     print(result)
+    
+def example_2():
+    '''
+    >>> example_2()
+    None
+    '''
+    hello_regex = re.compile(r'hello world')
+    result = hello_regex.match('blah blah hello world')
+    print(result)
+
+
+# We can use the `search` method to find a pattern in the middle of string
+def example_3():
+    '''
+    >>> example_3()
+    <_sre.SRE_Match object; span=(10, 21), match='hello world'>
+    '''
+    hello_regex = re.compile(r'hello world')
+    result = hello_regex.search('blah blah hello world')
+    print(result)
